f2f7d6e0f8139ee11846a31b0b396b96c6405d83
Yokke <mdembelepro@gmail.com>
2022-11-29 11:56:36 +0100

    update

--- config/connexion_bdd2.php
+++ config/connexion_bdd2.php
@@ -1,6 +1,7 @@
 <?php 
+define('HOST','sdsfdsfd');
 try{
-  $bdd2 = new PDO('mysql:host=localhost;dbname=api_snk','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+  $bdd2 = new PDO("mysql:host=localhost;dbname=api_snk",'root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 }
 
